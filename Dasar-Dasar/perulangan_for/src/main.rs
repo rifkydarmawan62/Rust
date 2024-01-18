53ae7766ac88fca287f60a1d4bde9846834689c0
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2024-01-18 20:52:41 +0700

    Menambahkan program

--- Dasar-Dasar/perulangan_for/src/main.rs
+++ Dasar-Dasar/perulangan_for/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    //Perulangan loop dengan range
+    for number in 1..=5 {
+        println!("Nomor = {}", number);
+    }
+
+    // Perulangan loop untuk iterator list
+    let daftar_buah = ["apel", "pisang", "ceri"];
+    for item_buah in daftar_buah.iter(){
+        println!("Buah {}", item_buah);
+    }
 }
